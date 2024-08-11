0fd84b2c6cacc843db281c1704bd6bb0a6cdc095
SwatiSrivastava <swati.srivastava@nagarro.com>
2024-08-11 20:45:32 +0530

    first commit

---
+++ jenkinsassignment/src/main/java/jenkinsassignment/JenkinsTest.java
@@ -0,0 +1,16 @@
+package jenkinsassignment;
+
+public class JenkinsTest {
+
+	public JenkinsTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is the test project");
+
+	}
+
+}
